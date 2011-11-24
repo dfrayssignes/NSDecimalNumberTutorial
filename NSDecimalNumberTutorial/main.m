4a2ba4bf8a4077d61482f38dccc1d4918cd7ed5a
Denis Frayssignes <dj@j2chess.com>
2011-11-24 21:51:38 +0000

    Initial Commit

---
+++ NSDecimalNumberTutorial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSDecimalNumberTutorial
+//
+//  Created by Denis Frayssignes on 24/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
